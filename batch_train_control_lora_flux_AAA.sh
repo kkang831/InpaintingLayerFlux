1e4cf43360af5b6a86cf40d2b6bb02a623ee5b70
kkang <kkang831@postech.ac.kr>
2025-06-06 05:50:12 +0900

    shadow_generation_network_stage1

--- batch_train_control_lora_flux_AAA.sh
+++ batch_train_control_lora_flux_AAA.sh
@@ -1,6 +1,3 @@
-
-
-
 #! /bin/sh
 
 #SBATCH -J train_control_lora_flux_AAA
@@ -9,11 +6,12 @@
 
 
 #### Select GPU
+#SBATCH -q hpgpu
 #SBATCH -p A100-80GB
 #SBATCH --gres=gpu:1
-#SBATCH   --nodes=1              # number of nodes 
-#SBATCH   --ntasks-per-node=1
-#SBATCH   --cpus-per-task=1
+#SBATCH --nodes=1              # number of nodes 
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
 
 echo "CUDA_HOME=$CUDA_HOME"
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
@@ -23,19 +21,19 @@ srun -l /bin/hostname
 srun -l /bin/pwd
 srun -l /bin/date
 
-# echo "Start"
-# echo "conda PATH "
+echo "Start"
 
-# echo "source $HOME/anaconda3/etc/profile.d/conda.sh"
-# source $HOME/anaconda3/etc/profile.d/conda.sh    #경로
+echo "source $HOME/anaconda3/etc/profile.d/conda.sh"
+source $HOME/anaconda3/etc/profile.d/conda.sh    #경로
 
-# echo "conda activate py36 "
-# conda activate py36          #사용할 conda env
+echo "conda activate pytorch260"
+conda activate pytorch260          #사용할 conda env
 
 # SAMPLES_DIR=$HOME/TensorFlow-2.x-Tutorials/
 # python3  $SAMPLES_DIR/03-Play-with-MNIST/main.py  # run할 파일(python)
 
-which python
+echo "which python3"
+which python3
 
 pyclean ./ && accelerate launch --config_file accelerate.yaml --gpu_ids 0 train_control_lora_flux_AAA.py \
     --pretrained_model_name_or_path black-forest-labs/FLUX.1-dev \
@@ -58,14 +56,10 @@ pyclean ./ && accelerate launch --config_file accelerate.yaml --gpu_ids 0 train_
     --gradient_accumulation_steps 8 \
     --max_train_steps 22000
 
-
-
 date
 
-# echo " conda deactivate py36  "
-
-# conda deactivate     # 마무리 deactivate
-
+echo " conda deactivate pytorch260"
+conda deactivate     # 마무리 deactivate
 
 date
 squeue  --job  $SLURM_JOBID
